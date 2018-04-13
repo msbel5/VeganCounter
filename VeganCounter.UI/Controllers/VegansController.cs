ebbedcd47384d639469c68f2162dc85b6325d243
ms bel <msbel5@gmail.com>
2018-04-14 01:21:52 +0300

    ipaddress

--- VeganCounter.UI/Controllers/VegansController.cs
+++ VeganCounter.UI/Controllers/VegansController.cs
@@ -58,7 +58,7 @@ namespace VeganCounter.UI.Controllers
             using (var reader = new DatabaseReader(AppDomain.CurrentDomain.BaseDirectory + "/App_Data/GeoLite2-City.mmdb"))
             {
                 var ipAddress = HttpContext.Request.UserHostAddress;
-                CityResponse location = reader.City("88.238.121.70");
+                CityResponse location = reader.City("ipAddress");
                 
                 if (_cim.Get(location.City.ToString()) != null)
                 {
